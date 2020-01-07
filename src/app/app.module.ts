5cd1a28b3868dcb04d9d4da0f34d656ff00e22fd
tim <tim.huang61@gmail.com>
2020-01-07 16:29:29 +0800

    Feature 8: APP_BOOTSTRAP_LISTENER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {APP_BOOTSTRAP_LISTENER, APP_INITIALIZER, ComponentRef, NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {CommonModule, Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
@@ -14,15 +14,23 @@ import {CommonModule, Location, LocationStrategy, PathLocationStrategy} from '@a
   ],
   providers: [Location,
     {provide: LocationStrategy, useClass: PathLocationStrategy},
-    {provide: APP_INITIALIZER, useFactory: runSettingsOnInit, multi: true}
+    {provide: APP_INITIALIZER, useFactory: runSettingsOnInit, multi: true},
+    {provide: APP_BOOTSTRAP_LISTENER, useFactory: runBootstrap, multi: true}
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
 
- // Feature 7: APP_INITIALIZER
+// Feature 7: APP_INITIALIZER
 export function runSettingsOnInit() {
   return () => console.log('app init');
+}
+
+// Feature 8: APP_BOOTSTRAP_LISTENER
+export function runBootstrap() {
+  return (component: ComponentRef<any>) => {
+    console.log(component.instance);
+  };
 }
 
