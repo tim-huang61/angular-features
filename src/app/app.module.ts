5f784fb9472429c04d3b7222ec1600497b3a1f75
tim <tim.huang61@gmail.com>
2020-01-07 16:25:03 +0800

    Feature 7: APP_INITIALIZER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {CommonModule, Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
@@ -12,9 +12,17 @@ import {CommonModule, Location, LocationStrategy, PathLocationStrategy} from '@a
     BrowserModule,
     CommonModule
   ],
-  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
+  providers: [Location,
+    {provide: LocationStrategy, useClass: PathLocationStrategy},
+    {provide: APP_INITIALIZER, useFactory: runSettingsOnInit, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+}
+
+ // Feature 7: APP_INITIALIZER
+export function runSettingsOnInit() {
+  return () => console.log('app init');
 }
 
