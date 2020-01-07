5ac9301cd8f5db2b34b8354ef0b3f71ae06ceefa
tim <tim.huang61@gmail.com>
2020-01-07 15:09:29 +0800

    Feature 3: interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // Feature 3 interpolation : replace {{}}
+  interpolation: ['[[', ']]']
 })
 export class AppComponent {
   name = 'angular-features';
 
-  constructor(private title: Title, private  meta: Meta) {
+  constructor(private title: Title, private  meta: Meta, private location: Location) {
     // Feature 1 Title.
     this.title.setTitle('wow! I have set title.');
     // Feature 2 Meta.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
